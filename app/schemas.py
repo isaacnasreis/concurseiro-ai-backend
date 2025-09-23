e9c31d3ea8ef31d6f99dedd80ea49864852b8907
Isaac N Reis <isaacreisn@gmail.com>
2025-09-22 22:03:43 -0300

    feat: creating enum for difficulty levels

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,11 @@
+from enum import Enum
 from pydantic import BaseModel, Field
 from typing import List
+
+class NivelDificuldade(str, Enum):
+    facil = "Fácil"
+    medio = "Médio"
+    dificil = "Difícil"
 
 class QuestaoRequest(BaseModel):
     """
@@ -16,10 +22,9 @@ class QuestaoRequest(BaseModel):
         description="O tópico específico dentro da matéria.",
         example="Atos Administrativos"
     )
-    nivel_dificuldade: str = Field(
-        "Médio",
-        description="O nível de dificuldade desejado para a questão.",
-        example="Fácil"
+    nivel_dificuldade: NivelDificuldade = Field(
+        default=NivelDificuldade.medio,
+        description="O nível de dificuldade desejado para a questão."
     )
 
 class QuestaoResponse(BaseModel):
